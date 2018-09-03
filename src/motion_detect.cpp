4744737df84891413eea5820749164999cdfe489
funkyfisch <funkyfisch@gmail.com>
2018-09-03 15:58:57 +0200

    updated switch statements for pointCameraTowards()

--- src/motion_detect.cpp
+++ src/motion_detect.cpp
@@ -73,19 +73,38 @@ Mat motionDetectBW(Mat oldFrame, Mat newFrame, Mat displayFrame, bool *motionDet
   return displayFrame;
 }
 
-void pointCameraTowards(int quarter) {
-  switch(quarter)
-  case 1:
-  //panright(),pitchup();
-  case 2:
-  //panright(),pitchdown();
-  case 3:
-  //panleft(), pitchdown();
-  case 4:
-  //panleft(), pitchup();
-  default:
+void pointCameraTowards(int region) {
+  switch(region) {
+    case 1:
+    //panleft(),pitchup();
+      break;
+    case 2:
+    //pitchup();
+      break;
+    case 3:
+    //panright(), pitchup();
+      break;
+    case 4:
+    //panleft();
+      break;
+    case 5:
+      break;
+    case 6:
+    //panright();
+      break;
+    case 7:
+    //panleft(), pitchdown();
+      break;
+    case 8:
+    //pitchdown();
+      break;
+    case 9:
+    //panright(), pitchdown();
+      break;
+    default:
+      break;
     return;
-  return;
+  }
 }
 
 //bool motionDetect(vector<Mat> oldRgb, vector<Mat> newRgb) {
